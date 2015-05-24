6cf6aee9e66cc4ecae8c97b9a7ec08115ab4490d
Kyo Nagashima <hail2u@gmail.com>
2015-05-24 23:17:39 +0900

    Lint with ESLint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,97 @@
-/*jshint node:true */
-'use strict';
+"use strict";
 
-var request = require('request');
+var request = require("request");
 
 module.exports = function (grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
 
     clean: {
-      test: ['tmp']
+      main: ["tmp"]
     },
 
     copy: {
-      test: {
-        src: ['_normalize.scss'],
-        dest: 'tmp/normalize.scss'
+      main: {
+        src: ["_normalize.scss"],
+        dest: "tmp/normalize.scss"
       }
     },
 
     sass: {
-      test: {
+      main: {
         options: {
           loadPath: process.cwd(),
-          sourcemap: 'none',
-          style: 'compressed'
+          sourcemap: "none",
+          style: "compressed"
         },
 
-        src: ['tmp/normalize.scss'],
-        dest: 'tmp/normalize.test.css'
+        src: ["tmp/normalize.scss"],
+        dest: "tmp/normalize.actual.css"
       }
     },
 
     cssmin: {
-      test: {
+      main: {
         options: {
-          keepSpecialComments: 0,
+          keepSpecialComments: 0
         },
 
         expand: true,
-        filter: 'isFile',
-        src: ['tmp/*.css']
+        filter: "isFile",
+        src: ["tmp/*.css"]
       }
     }
   });
 
-  grunt.util.linefeed = '\n';
+  grunt.util.linefeed = "\n";
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-sass");
 
-  grunt.registerTask('get_orig', function () {
-    var orig_url = 'http://necolas.github.com/normalize.css/latest/normalize.css';
-    var orig_file = 'tmp/normalize.orig.css';
+  grunt.registerTask("get", function () {
+    var url = "http://necolas.github.com/normalize.css/latest/normalize.css";
+    var file = "tmp/normalize.expected.css";
 
     var done = this.async();
-    request.get(orig_url, function (err, res, body) {
+    request.get(url, function (err, res, body) {
       if (!err && res.statusCode === 200) {
-        grunt.file.write(orig_file, body);
-        grunt.log.writeln('File "' + orig_file + '" created.');
+        grunt.file.write(file, body);
+        grunt.log.writeln('File "' + file + '" created.');
       }
 
       done(err);
     });
   });
 
-  grunt.registerTask('compare', function () {
-    var orig_file = 'tmp/normalize.orig.css';
-    var test_file = 'tmp/normalize.test.css';
-    var orig = grunt.file.read(orig_file);
-    var test = grunt.file.read(test_file);
+  grunt.registerTask("compare", function () {
+    var actual = {
+      path: "tmp/normalize.actual.css",
+      content: ""
+    };
+    var expected = {
+      path: "tmp/normalize.expected.css",
+      content: ""
+    };
+    actual.content = grunt.file.read(actual.path);
+    expected.content = grunt.file.read(expected.path);
 
-    if (orig !== test) {
-      grunt.fail.warn('File "' + test_file + '" does not equal file "' + orig_file + '".');
+    if (actual.content !== expected.content) {
+      grunt.fail.warn('File "' + actual.path + '" is not the same as file "' +
+        expected.path + '".');
     }
 
-    grunt.log.writeln('File "' + test_file + '" equals file "' + orig_file + '".');
+    grunt.log.writeln('File "' + actual.path + '" is the same as file "' +
+      expected.path + '".');
   });
 
-  grunt.registerTask('test', [
-    'clean:test',
-    'copy:test',
-    'sass:test',
-    'get_orig',
-    'cssmin:test',
-    'compare'
+  grunt.registerTask("test", [
+    "clean",
+    "copy",
+    "sass",
+    "get",
+    "cssmin",
+    "compare"
   ]);
 };
